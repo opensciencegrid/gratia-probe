1676b84d559972c8bb52eea581466b320745bdca
Brian Lin <brianhlin@gmail.com>
2019-01-09 14:04:17 -0600

    Initial unit tests for the HTCondor probe

--- test/test_condor_meter.py
+++ test/test_condor_meter.py
@@ -5,6 +5,7 @@ import classad
 
 from condor import condor_meter
 
+
 class CondorMeterTests(unittest.TestCase):
     """Unit tests for the HTCondor Gratia probe
     """
@@ -12,9 +13,17 @@ class CondorMeterTests(unittest.TestCase):
     def test_proc_expr(self):
         """get_num_procs() should be able to handle attributes set to ClassAd expressions
         """
-        self.fail()
+        for attr in ['MachineAttrCpus0', 'MATCH_EXP_JOB_GLIDEIN_Cpus', 'RequestCpus']:
+            jobad = classad.ClassAd()
+            jobad[attr] = classad.ExprTree('2 + 2')
+            procs = condor_meter.get_num_procs(jobad)
+            self.assertEquals(procs, 4)
 
     def test_proc_int(self):
         """The Processors field should always return an integer
         """
-        self.fail()
+        for attr in ['MachineAttrCpus0', 'MATCH_EXP_JOB_GLIDEIN_Cpus', 'RequestCpus']:
+            jobad = classad.ClassAd()
+            jobad[attr] = 'broken attribute'
+            procs = condor_meter.get_num_procs(jobad)
+            self.assertIsInstance(procs, int)
