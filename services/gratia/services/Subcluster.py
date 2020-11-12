ba731cbabbfe8d71eb136b8c512c319ccbfc0055
Carl Edquist <edquist@cs.wisc.edu>
2020-11-12 13:29:47 -0600

    add py2/3 compat for services/gratia/services/Subcluster.py
    SOFTWARE-4348

--- services/gratia/services/Subcluster.py
+++ services/gratia/services/Subcluster.py
@@ -85,7 +85,7 @@ class Subcluster(record.Record):
     def Timestamp(self, value):
         """ The time the record was gathered
         Expressed in number of second since epoch or a string formated using the format xsd:dateTime. """
-        if isinstance(value, types.StringType):
+        if isinstance(value, str):
             realvalue = value
         else:
             realvalue = utils.TimeToString(time.gmtime(value))
