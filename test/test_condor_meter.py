5d6c43ebd292fbd1f6d1b191d06a779bc9c74cdb
Brian Lin <blin@cs.wisc.edu>
2023-03-23 16:14:59 -0500

    Add privilege drop unit tests

--- test/test_condor_meter.py
+++ test/test_condor_meter.py
@@ -1,6 +1,7 @@
 #!/bin/env python
 
 import unittest
+from unittest.mock import patch
 import classad
 
 from condor_ap import condor_meter
@@ -35,3 +36,63 @@ class CondorMeterTests(unittest.TestCase):
             jobad[attr] = 'broken attribute'
             procs = condor_meter.get_num_procs(jobad)
             self.assertIsInstance(procs, int)
+
+
+class BecomeCondorTests(unittest.TestCase):
+    """Unit tests for the become_condor function
+    """
+
+    def setUp(self):
+        self.mock_getpwnam = patch('pwd.getpwnam').start()
+        self.mock_setgid = patch('os.setgid').start()
+        self.mock_setuid = patch('os.setuid').start()
+
+        # specific pwd.struct_passwd instance
+        self.mock_user = self.mock_getpwnam.return_value
+        self.mock_user.pw_gid = 9618
+        self.mock_user.pw_uid = 9619
+
+    def test_success(self):
+        """Everything is as expected
+        """
+        condor_meter.become_condor()
+
+        self.mock_getpwnam.assert_called_once_with('condor')
+        self.mock_setgid.assert_called_once_with(self.mock_user.pw_gid)
+        self.mock_setuid.assert_called_once_with(self.mock_user.pw_uid)
+
+    def test_no_condor_user(self):
+        """There is no 'condor' user
+        """
+        self.mock_getpwnam.configure_mock(side_effect=KeyError)
+
+        with self.assertRaises(condor_meter.utils.InternalError):
+            condor_meter.become_condor()
+
+        self.mock_getpwnam.assert_called_once_with('condor')
+        self.mock_setgid.assert_not_called()
+        self.mock_setuid.assert_not_called()
+
+    def test_fail_drop_gid(self):
+        """Can't setgid
+        """
+        self.mock_setgid.configure_mock(side_effect=PermissionError)
+
+        with self.assertRaises(condor_meter.utils.InternalError):
+            condor_meter.become_condor()
+
+        self.mock_getpwnam.assert_called_once_with('condor')
+        self.mock_setgid.assert_called_once_with(self.mock_user.pw_gid)
+        self.mock_setuid.assert_not_called()
+
+    def test_fail_drop_uid(self):
+        """Can't setgid
+        """
+        self.mock_setuid.configure_mock(side_effect=PermissionError)
+
+        with self.assertRaises(condor_meter.utils.InternalError):
+            condor_meter.become_condor()
+
+        self.mock_getpwnam.assert_called_once_with('condor')
+        self.mock_setgid.assert_called_once_with(self.mock_user.pw_gid)
+        self.mock_setuid.assert_called_once_with(self.mock_user.pw_uid)
