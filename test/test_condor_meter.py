2234b0b131bcd32232946f99858111ace69327ed
Brian Lin <blin@cs.wisc.edu>
2023-03-24 16:27:26 -0500

    Add priv drop tests for the CONDOR_IDS case (SOFTWARE-5531)

--- test/test_condor_meter.py
+++ test/test_condor_meter.py
@@ -47,6 +47,11 @@ class BecomeCondorTests(unittest.TestCase):
         self.mock_setgid = patch('os.setgid').start()
         self.mock_setuid = patch('os.setuid').start()
 
+        # ridiculous looking nonsense to be able to mock out htcondor.param['FOO']
+        self.htcondor_config = {}
+        self.mock_htcondor_param = patch('htcondor.param',
+                                         **{'__getitem__.side_effect': self.htcondor_config.__getitem__}).start()
+
         # specific pwd.struct_passwd instance
         self.mock_user = self.mock_getpwnam.return_value
         self.mock_user.pw_gid = 9618
@@ -56,8 +61,9 @@ class BecomeCondorTests(unittest.TestCase):
         self.mock_getpwnam.stop()
         self.mock_setgid.stop()
         self.mock_setuid.stop()
+        self.mock_htcondor_param.stop()
 
-    def test_success(self):
+    def test_condor_user_success(self):
         """Everything is as expected
         """
         condor_meter.become_condor()
@@ -101,3 +107,35 @@ class BecomeCondorTests(unittest.TestCase):
         self.mock_getpwnam.assert_called_once_with('condor')
         self.mock_setgid.assert_called_once_with(self.mock_user.pw_gid)
         self.mock_setuid.assert_called_once_with(self.mock_user.pw_uid)
+
+    def test_condor_ids(self):
+        """Test privilege drop when admins specify CONDOR_IDS
+        """
+        self.htcondor_config['CONDOR_IDS'] = '1.2'
+
+        condor_meter.become_condor()
+
+        self.mock_setgid.assert_called_once_with(2)
+        self.mock_setuid.assert_called_once_with(1)
+
+    def test_malformed_condor_ids(self):
+        """Test error handling for malformed CONDOR_IDS configuration
+        """
+        for bad_id in ('i love condor', '', '100', '2.gratia', 'garbage.50'):
+            self.htcondor_config['CONDOR_IDS'] = bad_id
+            with self.assertRaises(condor_meter.utils.InternalError):
+                condor_meter.become_condor()
+
+    @patch('htcondor.reload_config')
+    @patch('os.environ.setdefault')
+    def test_htcondor_ce_ids(self, mock_environ, mock_reload):
+        """Test privilege drop when an HTCondor-CE admin specifies CONDOR_IDS
+        """
+        self.htcondor_config['CONDOR_IDS'] = '1.2'
+
+        condor_meter.become_condor('htcondor-ce')
+
+        mock_environ.assert_called_once_with('CONDOR_CONFIG', '/etc/condor-ce/condor_config')
+        mock_reload.assert_called_once()
+        self.mock_setgid.assert_called_once_with(2)
+        self.mock_setuid.assert_called_once_with(1)
