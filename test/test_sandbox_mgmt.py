46d3c94ec99690055d03d12c885d51fdc6a1fbbe
Jeff Takaki <jtakaki@morgridge.org>
2023-07-31 11:56:37 -0500

    Add unit test for Compress Outbox function

--- test/test_sandbox_mgmt.py
+++ test/test_sandbox_mgmt.py
@@ -1,6 +1,8 @@
 #!/bin/env python
 
 import os
+import shutil
+import tempfile
 import unittest
 from unittest.mock import patch, PropertyMock
 
@@ -32,3 +34,34 @@ class SandboxMgmtTests(unittest.TestCase):
             except (FileNotFoundError, NameError):
                 # don't need to clean up what's not there
                 pass
+
+class CompressOutboxTests(unittest.TestCase):
+    def setUp(self):
+        #provision test environment
+        self.probe_dir = tempfile.mkdtemp()
+        self.outbox = os.path.join(self.probe_dir, 'outbox')
+        os.makedirs(self.outbox, exist_ok=True)
+        self.outfiles = ['testfile1', 'testfile2']
+        # add content to the files
+        for fname in self.outfiles:
+            with open(os.path.join(self.outbox, fname), 'w') as f:
+                f.write('test content')
+
+    def tearDown(self):
+        #Remove probe_dir after test
+        shutil.rmtree(self.probe_dir)
+
+    @patch('gratia.common.config.ConfigProxy.get_GratiaExtension', create=True, return_value ='test-extension')
+    @patch('gratia.common.config.ConfigProxy.getFilenameFragment', create=True, return_value ='test-filename')
+    def test_compress_outbox(self, mock_gratia_ext, mock_file_frag):
+        """CompressOutbox compresses the files in the outbox directory
+            and stores the resulting tarball in probe_dir/staged.
+        """
+        #Parameters for function
+        probe_dir = self.probe_dir
+        outbox = self.outbox
+        outfiles = self.outfiles
+
+        #Assert that CompressOutbox returns True
+        result = sandbox_mgmt.CompressOutbox(probe_dir, outbox, outfiles)
+        self.assertTrue(result)
